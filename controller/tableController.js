7aa4749653f9c8c09c3dfa8dc47c250856139218
Utkarsh gubrelay <utkarshgubrelay2000@gmail.com>
2021-06-26 19:10:43 +0530

    table status

--- controller/tableController.js
+++ controller/tableController.js
@@ -1,14 +1,35 @@
 const Table = require("../model/TableModel");
 
-exports.createTable=(req,res)=>{
-    const { capacity,mergeable,current_status } = req.body;
-    let newTable=new Table({
-      capacity:capacity,mergeable:mergeable,current_status:current_status
-    })
-    newTable.save().then(found => {
-   res.json({error:false,data:"Successfully"})
-    }).catch(err => {
-      res.status(503).json({error:true,data:'Something went wrong with db',errMsg:err})
-    })
-  
-  }
+exports.createTable = (req, res) => {
+  const { capacity, mergeable, current_status } = req.body;
+  let newTable = new Table({
+    capacity: capacity, mergeable: mergeable, current_status: current_status
+  })
+  newTable.save().then(found => {
+    res.json({ error: false, data: "Successfully" })
+  }).catch(err => {
+    res.status(503).json({ error: true, data: 'Something went wrong with db', errMsg: err })
+  })
+
+}
+exports.editTable = (req, res) => {
+  const { capacity, mergeable, current_status } = req.body;
+  Table.findByIdAndUpdate(req.params.id, {
+    capacity: capacity, mergeable: mergeable, current_status: current_status
+  }).then(found => {
+    res.json({ error: false, data: "Updated" })
+  }).catch(err => {
+    res.status(503).json({ error: true, data: 'Something went wrong with db', errMsg: err })
+  })
+
+}
+exports.changeTableStatus = (req, res) => {
+const {id,status}=req.query
+  Table.findByIdAndUpdate(id, {
+    current_status: status
+  }).then(found => {
+    res.json({ error: false, data: "Updated" })
+  }).catch(err => {
+    res.status(503).json({ error: true, data: 'Something went wrong with db', errMsg: err })
+  })
+}
--- routes/table.js
+++ routes/table.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var tableController=require('../controller/tableController')
 
-r
+
 router.post('/createTable',tableController.createTable,err=>{
+    console.log('error while signup user')
+  })
+  router.put('/editTable',tableController.editTable,err=>{
+    console.log('error while signup user')
+  })
+  router.post('/changeTableStatus?',tableController.changeTableStatus,err=>{
     console.log('error while signup user')
   })
 // router.get('/getNearby',restaurantController.getNearby,err=>{
