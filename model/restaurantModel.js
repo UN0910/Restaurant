8566a4d1ab2d6127d4a73b0ab4c33d7175d5d30b
Utkarsh gubrelay <utkarshgubrelay2000@gmail.com>
2021-06-26 16:13:05 +0530

    resto model

--- model/restaurantModel.js
+++ model/restaurantModel.js
@@ -1,22 +1,32 @@
 const mongoose = require("mongoose");
 const RestaurantSchema = new mongoose.Schema({
-//     restaurant_floors: {
-//     type: Array,
-//   },
-//   mergeable: {
-//     type: Boolean,
-//   },
-//   restaurant_name:{
-//       type:String
-//   },
-//   address:{
-//       type:String
-//   },
-  location: {
-    type: { type: String },
-    coordinates: []
-   },
-});
+    restaurant_floors: [{_id:false, floor: { type: mongoose.Schema.Types.ObjectId, ref: 'Floor' } }],
+    reviews: [{_id:false, review: { type: mongoose.Schema.Types.ObjectId, ref: 'Review' } }],
+    mergeable: {
+        type: Boolean,
+    },
+    restaurant_name: {
+        type: String
+    },
+    current_rating:{type:Number},
+    menuList:{ _id:false,pdf_link:{type:String} },
+    address: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    thumbnail_images: [],
+    restaurant_images: [],
+    opening_timings:[{_id:false,day:{type:String},timeslots:[]}],
+    cuisines: [],
+    location: {
+        type: { type: String },
+        coordinates: []
+    },
+    
+},
+{ timestamps: true });
 RestaurantSchema.index({ location: "2dsphere" });
 const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
 module.exports = Restaurant;
