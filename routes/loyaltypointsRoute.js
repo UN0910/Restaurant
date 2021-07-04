4b322d61f982844dbd1cc38c0052cfcf19433a03
Jess70 <patidarjayesh27@gmail.com>
2021-07-05 02:20:48 +0530

    minor file change

--- routes/loyaltypointsRoute.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-const loyaltyController = require("../controller/loyaltypointsController");
-const router = express.Router();
-
-router.route("/user/loyaltypoints/:id").get(loyaltyController.getLoyaltyPoints);
-router.route("/user/loyaltypoints").post(loyaltyController.LoyaltyPoints);
-
-module.exports = router;
